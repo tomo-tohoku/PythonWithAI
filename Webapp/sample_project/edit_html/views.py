ed8311d4af8ba9358f31b323d7fd9cce6a10d62e
tomo <naorietomo@gmail.com>
2026-05-26 02:45:41 +0900

    views.pyを動作するように変更

--- Webapp/sample_project/edit_html/views.py
+++ Webapp/sample_project/edit_html/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import EditForm
+from django.http import HttpResponse
+
 from bs4 import BeautifulSoup
 
 from playwright.sync_api import sync_playwright
@@ -39,7 +41,6 @@ def result(request):
         # フォームデータ取得
         url = request.POST['url']
 
-
         # sync_playwright()の終了処理で「Playwrightエンジン全体」を終了させ、ブラウザも終了させる
         with sync_playwright() as p:
             # ブラウザの起動
@@ -72,27 +73,39 @@ def result(request):
                     params['error_msg'] = f"エラー：サイトが見つかりません\nステータスコード：{response.status}"
                     return render(request, 'edit_html/index.html', params)
 
-                # time.sleep(2) # あまり良くない
+                # time.sleep(2) # time.sleep(2) はあまり良くない
                 page.wait_for_load_state('networkidle') # 追記部分
 
-                full_html = page.content()
+                full_html = page.content() # page.content() の返り値は str
+                # print(full_html) # デバッグ用
 
-                with open(INTERMEDIATE_FILE_PATH, mode = "w", encoding = "utf-8") as f:
+                # w+ で新規作成して読み書き
+                with open(INTERMEDIATE_FILE_PATH, mode = "w+", encoding = "utf-8") as f:
                     f.write(full_html)
-                with open(INTERMEDIATE_FILE_PATH, mode = "r", encoding = "utf-8") as f1:
-                    intermediate_html = f1.read()
-                    with open(RESULT_FILE_PATH, mode = "w", encoding = "utf-8") as f2:
-                        # soup = BeautifulSoup(intermediate_html, "html.parser") # html.parser は Python標準で解析性能が弱い 
-                        soup = BeautifulSoup(intermediate_html, "lxml") # lxml の方がいい　追記（5/26）
-                        custom_style = f"""
-                        <style>
-                        {request.POST['style_area']}
-                        </style>                     
-                        """
-                        if soup.body:
-                            soup.body.append(BeautifulSoup(custom_style, "html.parser"))
-                        f2.write(str(soup))
-                        return render(request, 'edit_html/result.html', params)
+                    f.flush() # バッファ内容を強制的にファイルへ書き込む
+                    f.seek(0) # ファイルポインタを先頭へ移動する
+                    intermediate_html = f.read()
+                    # print(intermediate_html) # デバッグ用
+                with open(RESULT_FILE_PATH, mode = "w", encoding = "utf-8") as f:
+                    # soup = BeautifulSoup(intermediate_html, "html.parser") # html.parser は Python標準で解析性能が弱い 
+                    soup = BeautifulSoup(intermediate_html, "lxml") # lxml の方がいい　追記（5/26）
+                    custom_style = request.POST.get('style_area', '')
+                    if custom_style and soup.body: # custom_style が空文字でなく、かつ body タグが存在するなら
+                        style_tag = soup.new_tag('style')
+                        style_tag.string = custom_style
+                        soup.body.append(style_tag)
+
+                    # 以前の書き方
+                    # if soup.body: # body タグが存在するなら
+                    #     soup.body.append(BeautifulSoup(custom_style, "lxml"))
+
+                    # 以前の書き方
+                    # f.write(str(soup))
+                    # return render(request, 'edit_html/result.html', params)
+
+                    # 新しい書き方（5/26）
+                    html = str(soup)
+                    return HttpResponse(html)
             except Exception as e:
                 params['error_msg'] = f"エラー：予期せぬエラー\nエラーの詳細：\n{e}"
             finally:
